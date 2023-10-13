720ae904e2a010591574522e2e405bdf232cf475
Tejabilla <147588304+Tejabilla@users.noreply.github.com>
2023-10-13 14:42:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-785db401"
+  ami           = "ami-041feb57c611358bd"
   instance_type = "t2.micro"
 }
